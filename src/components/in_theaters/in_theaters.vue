a9aebe13e3b141ebc0813c7f1bd315e0befb80b7
mohong <522916411@qq.com>
2016-12-05 16:13:10 +0800

    详情页打开和关闭实现

--- src/components/in_theaters/in_theaters.vue
+++ src/components/in_theaters/in_theaters.vue
@@ -1,37 +1,54 @@
 <template>
     <div class="in_theaters">
         <ul class="movielist">
-            <li class="movie-item" v-for="movie in movies"><movie :movie="movie"></movie></li>
+            <li @click="showDetail(movie,$event)" class="movie-item" v-for="movie in movies"><movie :movie="movie"></movie></li>
             <div class="clearfix"></div>
         </ul>
+    </div>
+    <div class="detail-wrapper">
+        <movie-detail :movie="selectMovie" v-ref:detail></movie-detail>
     </div>
 </template>
 
 <script>
     import movie from '../movie/movie.vue';
+    import movieDetail from '../movie_detail/movie_detail.vue';
 
     export default {
         data() {
           return {
-              movies: []
+              movies: [],
+              selectMovie: {}
           };
         },
         created() {
             this.$http.jsonp('https://api.douban.com//v2/movie/in_theaters').then((response) => {
                 this.movies = response.body.subjects;
-                console.log(this.movies);
             });
         },
+        methods: {
+            showDetail(movie, event) {
+                this.selectMovie = movie;
+                this.$refs.detail.show();
+            }
+        },
         components: {
-            movie
+            movie,
+            movieDetail
         }
     };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
     .in_theaters
+        position: absolute
+        left: 0
+        top: 240px
+        bottom: 0
+        overflow: hidden
         .movielist
             width: 350px
+            height: 100%
             margin: 0 auto
             .movie-item
                 float: left
@@ -40,4 +57,9 @@
                 clear: both
                 width: 100%
                 height: 20px
+        movie-detail
+            color: red
+    .fade-transition
+        opacity: 1
+
 </style>
---
+++ src/components/movie_detail/movie_detail.vue
@@ -0,0 +1,42 @@
+<template>
+    <div v-show="showFlag" class="movie-detail" transition="move">
+        <h1 @click="show"><</h1>
+        {{movie.title}}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+          movie: {
+              type: Object
+          }
+        },
+        data() {
+            return {
+                showFlag: false
+            };
+        },
+        methods: {
+            show() {
+                this.showFlag = !this.showFlag;
+            }
+        }
+    };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    .movie-detail
+        top: 0
+        left: 0
+        position: absolute
+        width: 100%
+        height: 100%
+        overflow: hidden
+        background: #fff
+        &.move-transition
+            transition: all 0.2s linear
+            transform: translate3d(0, 0, 0)
+        &.move-enter,&.move-leave
+            transform: translate3d(100%, 0, 0)
+</style>
