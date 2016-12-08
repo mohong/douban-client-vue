575dcd03ae645741c39ab0604db71faed81333df
mohong <522916411@qq.com>
2016-12-08 22:34:09 +0800

    移除头部banner组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        <v-header></v-header>
         <div class="tab">
             <div class="tab-item">
                 <a v-link="{path: '/in_theaters'}">热播</a>
@@ -17,24 +16,11 @@
 </template>
 
 <script>
-    import header from './components/header/header';
-    import movie from './components/movie/movie';
-
     export default {
         data() {
             return {
                 movies: []
             };
-        },
-        created() {
-            this.$http.get('http://127.0.0.1:4000/').then((response) => {
-                this.movies = response.body;
-                // console.log(this.movies);
-            });
-        },
-        components: {
-            movie,
-            'v-header': header
         }
     };
 </script>
--- src/components/in_theaters/in_theaters.vue
+++ src/components/in_theaters/in_theaters.vue
@@ -1,65 +1,24 @@
 <template>
     <div class="in_theaters">
-        <ul class="movielist">
-            <li @click="showDetail(movie,$event)" class="movie-item" v-for="movie in movies"><movie :movie="movie"></movie></li>
-            <div class="clearfix"></div>
-        </ul>
-    </div>
-    <div class="detail-wrapper">
-        <movie-detail :movie="selectMovie" v-ref:detail></movie-detail>
+        正在热播
     </div>
 </template>
 
 <script>
-    import movie from '../movie/movie.vue';
-    import movieDetail from '../movie_detail/movie_detail.vue';
-
     export default {
         data() {
           return {
-              movies: [],
-              selectMovie: {}
+              movies: []
           };
         },
         created() {
             this.$http.jsonp('https://api.douban.com//v2/movie/in_theaters').then((response) => {
                 this.movies = response.body.subjects;
             });
-        },
-        methods: {
-            showDetail(movie, event) {
-                this.selectMovie = movie;
-                this.$refs.detail.show();
-            }
-        },
-        components: {
-            movie,
-            movieDetail
         }
     };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-    .in_theaters
-        position: absolute
-        left: 0
-        top: 240px
-        bottom: 0
-        overflow: hidden
-        .movielist
-            width: 350px
-            height: 100%
-            margin: 0 auto
-            .movie-item
-                float: left
-                margin: 2px
-            .clearfix
-                clear: both
-                width: 100%
-                height: 20px
-        movie-detail
-            color: red
-    .fade-transition
-        opacity: 1
 
 </style>
