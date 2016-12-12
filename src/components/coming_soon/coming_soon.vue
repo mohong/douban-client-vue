66a5d63cfc7979e368d62b60db44602fb4c571ae
mohong <522916411@qq.com>
2016-12-12 21:11:02 +0800

    热映，期待，top250完成

--- src/components/coming_soon/coming_soon.vue
+++ src/components/coming_soon/coming_soon.vue
@@ -1,11 +1,59 @@
 <template>
-    <div>即将上映</div>
+    <div class="coming_soon">
+        <div class="coming_soon-wrapper" v-el:coming_soon>
+            <ul>
+                <li @click="showMovie(movie)" v-for="movie in movies"><card :movie="movie"></card></li>
+            </ul>
+        </div>
+        <movie :movie="selectMovie" v-ref:movie></movie>
+    </div>
 </template>
 
 <script>
-    export default {};
+    import card from '../card/card.vue';
+    import movie from '../movie/movie.vue';
+    import BScroll from 'better-scroll';
+
+    export default {
+        data() {
+            return {
+                movies: [],
+                selectMovie: {}
+            };
+        },
+        created() {
+            this.$http.jsonp('https://api.douban.com/v2/movie/coming_soon').then((response) => {
+                this.movies = response.body.subjects;
+                this.$nextTick(() => {
+                    this._initScroll();
+                });
+            });
+        },
+        methods: {
+            _initScroll() {
+                this.coming_sooncroll = new BScroll(this.$els.coming_soon, {
+                    click: true
+                });
+            },
+            showMovie(movie) {
+                this.selectMovie = movie;
+                this.$refs.movie.show();
+            }
+        },
+        components: {
+            card,
+            movie
+        }
+    };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+    .coming_soon
+        .coming_soon-wrapper
+            position: absolute
+            display: block
+            width: 100%
+            top: 40px
+            bottom: 10px
+            overflow: hidden
 </style>
--- src/components/top250/top250.vue
+++ src/components/top250/top250.vue
@@ -1,11 +1,59 @@
 <template>
-    <div>top250</div>
+    <div class="top250">
+        <div class="top250-wrapper" v-el:top250>
+            <ul>
+                <li @click="showMovie(movie)" v-for="movie in movies"><card :movie="movie"></card></li>
+            </ul>
+        </div>
+        <movie :movie="selectMovie" v-ref:movie></movie>
+    </div>
 </template>
 
 <script>
-    export default {};
+    import card from '../card/card.vue';
+    import movie from '../movie/movie.vue';
+    import BScroll from 'better-scroll';
+
+    export default {
+        data() {
+            return {
+                movies: [],
+                selectMovie: {}
+            };
+        },
+        created() {
+            this.$http.jsonp('https://api.douban.com/v2/movie/top250').then((response) => {
+                this.movies = response.body.subjects;
+                this.$nextTick(() => {
+                    this._initScroll();
+                });
+            });
+        },
+        methods: {
+            _initScroll() {
+                this.top250croll = new BScroll(this.$els.top250, {
+                    click: true
+                });
+            },
+            showMovie(movie) {
+                this.selectMovie = movie;
+                this.$refs.movie.show();
+            }
+        },
+        components: {
+            card,
+            movie
+        }
+    };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+    .top250
+        .top250-wrapper
+            position: absolute
+            display: block
+            width: 100%
+            top: 40px
+            bottom: 10px
+            overflow: hidden
 </style>
