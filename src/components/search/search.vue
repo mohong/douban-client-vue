944e0e9a95c8e6a4d7597a5676c37c5962f31c1b
mohong <522916411@qq.com>
2016-12-14 13:47:45 +0800

    搜索组件整体完成

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,36 +1,48 @@
 <template>
     <div class="search">
         <input type="text" placeholder="输入电影名称" onfocus="false" v-model="movieTitle">
-        <div class="result">
-            <ul>
-                <li v-for="movie in searchResult">{{movie.title}}</li>
-            </ul>
-        </div>
+        <ul v-show="searchResult==''?false:true">
+            <li @click="selectMovie(movie)" v-for="movie in searchResult">{{movie.title}}</li>
+        </ul>
     </div>
+    <movie v-if="selectedMovie !== ''" :movie="selectedMovie" v-ref:movie></movie>
 </template>
 
 <script>
+    import movie from '../movie/movie.vue';
+
     export default {
         data() {
             return {
                 movieTitle: '',
-                searchResult: ''
+                searchResult: '',
+                selectedMovie: ''
             };
         },
         watch: {
             'movieTitle'(val, oldVal) {
-                this.searchResult = '';
-                this.searchMovie(val);
+                val === '' ? this.searchResult = '' : this.searchMovie(val);
             }
         },
         methods: {
             searchMovie(title) {
                 if (title !== '') {
                     this.$http.get('http://192.168.1.111:4000/api/movie/search/' + title).then((response) => {
-                        this.searchResult = response.body;
+                        if (response.body === '无相关资源') {
+                            this.searchResult = '';
+                        } else {
+                            this.searchResult = response.body;
+                        }
                     });
                 }
+            },
+            selectMovie(movie) {
+                this.selectedMovie = movie;
+                this.$refs.movie.show();
             }
+        },
+        components: {
+            movie
         }
     };
 </script>
@@ -46,4 +58,16 @@
             outline: none
             &::-webkit-input-placeholder
                 text-align: center
+        ul
+            width: 86%
+            height: 100%
+            margin: 0 auto
+            padding: 10px
+            border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+            border-radius: 20px
+            background-color: #fff
+            transition: all 1s
+        li
+            text-align: left
+            padding: 6px
 </style>
